81e6b624ab48a001c8c5e4603ebdde17c8e97e67
Zlatan <zlajox@gmail.com>
2017-04-26 22:27:33 +0200

    Added rest

--- ChatApp/src/controller/UserChatControllerImpl.java
+++ ChatApp/src/controller/UserChatControllerImpl.java
@@ -2,18 +2,27 @@ package controller;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
 import exceptions.AliasExistsException;
 import model.Host;
 
+@Path("/userChatController")
 public class UserChatControllerImpl implements UserChatController {
 
 	@Override
+	@POST
+	@Path("/register")
 	public List<Host> register(String address, String alias) throws AliasExistsException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@GET
+	@Path("/unregister")
 	public void unregister(Host host) {
 		// TODO Auto-generated method stub
 		
--- UserApp/src/controller/UserControllerImpl.java
+++ UserApp/src/controller/UserControllerImpl.java
@@ -2,31 +2,44 @@ package controller;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
 import exceptions.InvalidCredentialsException;
 import exceptions.UsernameExistsException;
 import model.User;
 
+@Path("/user")
 public class UserControllerImpl implements UserController {
 
 	@Override
+	@POST
+	@Path("/register")
 	public User register(String username, String password) throws UsernameExistsException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@GET
+	@Path("/login")
 	public Boolean login(String username, String password) throws InvalidCredentialsException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@GET
+	@Path("/logout")
 	public Boolean logout(User logout) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@GET
+	@Path("/allUsers")
 	public List<User> getAllUsers() {
 		// TODO Auto-generated method stub
 		return null;
